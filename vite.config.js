caa7fb916f2cf2a00a7c8fb7f45fa409b9b2905a
Olivier Goulet <olivier_goulet@icloud.com>
2024-02-21 23:49:35 -0500

    fix: use proper package name

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, './src/index.ts'),
-      name: 'index',
-      fileName: 'index',
+      name: 'ui',
+      fileName: 'ui',
     },
   },
 })
