74e649e2b1a0ec37562614fabdb8a8058dc9cd8e
Olivier Goulet <olivier_goulet@icloud.com>
2024-02-21 21:50:56 -0500

    feat: add resizable components (#13)

---
+++ src/components/ResizablePanel.tsx
@@ -0,0 +1,8 @@
+"use client"
+
+import * as ResizablePrimitive from "react-resizable-panels"
+import { cn } from "src/utils"
+
+const ResizablePanel = ResizablePrimitive.Panel
+
+export { ResizablePanel }
---
+++ src/components/ResizablePanelGroup.tsx
@@ -0,0 +1,19 @@
+"use client"
+
+import * as ResizablePrimitive from "react-resizable-panels"
+import { cn } from "src/utils"
+
+const ResizablePanelGroup = ({
+  className,
+  ...props
+}: React.ComponentProps<typeof ResizablePrimitive.PanelGroup>) => (
+  <ResizablePrimitive.PanelGroup
+    className={cn(
+      "flex h-full w-full data-[panel-group-direction=vertical]:flex-col",
+      className
+    )}
+    {...props}
+  />
+)
+
+export { ResizablePanelGroup }
